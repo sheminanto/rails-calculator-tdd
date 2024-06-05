72ba206ed8344f0610b36aabead137e0475df8e5
sheminanto <sheminanto@gmail.com>
2024-06-05 17:34:54 +0530

    Update addition service to allow new line as delimiter

--- app/services/addition_service.rb
+++ app/services/addition_service.rb
@@ -8,6 +8,12 @@ class AdditionService
   def process
     return 0 if @input_string.empty?
 
-    @input_string.split(',').map(&:to_i).reduce(&:+)
+    @input_string.split(Regexp.union(delimiters)).map(&:to_i).reduce(&:+)
+  end
+
+  private
+
+  def delimiters
+    ["\n", ',']
   end
 end
