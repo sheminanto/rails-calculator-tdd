fba1d6162d9556c4934de458e6d509f1f1ac79e5
sheminanto <sheminanto@gmail.com>
2024-06-05 18:26:06 +0530

    Update addition service to allow cusotm delmiter

--- app/services/addition_service.rb
+++ app/services/addition_service.rb
@@ -8,12 +8,21 @@ class AdditionService
   def process
     return 0 if @input_string.empty?
 
-    @input_string.split(Regexp.union(delimiters)).map(&:to_i).reduce(&:+)
+    delimiters, string = parse_delimiter(@input_string)
+    string.split(Regexp.union(delimiters)).map(&:to_i).reduce(&:+)
   end
 
   private
 
-  def delimiters
+  def parse_delimiter(string)
+    return [default_delimiters, string] unless string.start_with?('//')
+
+    delimiters = string.match(/\/\/(.+)\\n/)[1]
+    new_input_string = @input_string.split("\\n",2)[1]
+    [delimiters, new_input_string]
+  end
+
+  def default_delimiters
     ["\n", ',']
   end
 end
