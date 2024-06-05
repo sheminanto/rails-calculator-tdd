d8c7496f8371c6a39b039ff50cde1575194e345d
sheminanto <sheminanto@gmail.com>
2024-06-05 17:16:56 +0530

    Add route for calculations controller add action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-
+    resources :calculations, only: [] do
+      post :add, on: :collection
+    end
   end
 
   root "home#index"
