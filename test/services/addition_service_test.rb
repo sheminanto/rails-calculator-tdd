0eeb19232d4d81c175df20ce1da7ec16bbd11a96
sheminanto <sheminanto@gmail.com>
2024-06-05 17:22:06 +0530

    Update addition service test to check for zero when the input string is empty

--- test/services/addition_service_test.rb
+++ test/services/addition_service_test.rb
@@ -7,4 +7,9 @@ class AdditionServiceTest < ActiveSupport::TestCase
     sum = AdditionService.new('1,2,3').process
     assert_equal 6, sum
   end
+
+  def test_should_return_zero_if_the_string_is_empty
+    sum = AdditionService.new('').process
+    assert_equal 0, sum
+  end
 end
