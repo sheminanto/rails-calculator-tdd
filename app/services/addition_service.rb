dd07f92cc1699b0bef55e081b30e864c71af405e
sheminanto <sheminanto@gmail.com>
2024-06-17 15:59:45 +0530

    Update addition service to raise negative number exception

--- app/services/addition_service.rb
+++ app/services/addition_service.rb
@@ -42,7 +42,11 @@ class AdditionService
 
   def multiply(string, delimiters)
     numbers = get_numbers_from_string(string, delimiters)
-    numbers.reduce(&:*)
+    negative_numbers = numbers.select(&:negative?)
+
+    raise NegativeNumberException, negative_numbers if negative_numbers.any?
+
+    numbers.reduce { |product, number| number > 1000 ? product : product * number }
   end
 
   def get_numbers_from_string(string, delimiters)
