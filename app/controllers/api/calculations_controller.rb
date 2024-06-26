02694ddb255b1e6b6d2d041fec777360762069ad
sheminanto <sheminanto@gmail.com>
2024-06-26 10:45:31 +0530

    Add calculation param to calculations controller

--- app/controllers/api/calculations_controller.rb
+++ app/controllers/api/calculations_controller.rb
@@ -2,10 +2,16 @@
 
 class Api::CalculationsController < ApplicationController
   def add
-    result = AdditionService.new(params['input']).process
+    result = AdditionService.new(calculation_params['input'], calculation_params['action']).process
 
     render json: { result: result }
   rescue NegativeNumberException => e
     render json: { message: e.message }, status: :unprocessable_entity
+  end
+
+  private
+
+  def calculation_params
+    params.require(:calculation).permit(:input, :action)
   end
 end
--- test/controllers/api/calculations_controller_test.rb
+++ test/controllers/api/calculations_controller_test.rb
@@ -4,14 +4,14 @@ require 'test_helper'
 
 class Api::CalculationsControllerTest < ActionDispatch::IntegrationTest
   def test_should_perform_addition_and_return_result
-    post add_api_calculations_path, params: { input: '1,2' }
+    post add_api_calculations_path, params: { calculation: { input: '1,2'} }
     assert_response :success
     response_json = JSON.parse(response.body)
     assert_equal 3, response_json['result']
   end
 
   def test_should_return_error_when_negative_number_present
-    post add_api_calculations_path, params: { input: '1,2,-1' }
+    post add_api_calculations_path, params: { calculation: { input: '1,2,-1' } }
     assert_response :unprocessable_entity
     response_json = JSON.parse(response.body)
     assert_equal response_json['message'], 'negative numbers not allowed -1'
